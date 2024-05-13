eb15ccfcf92a8e45233e267ae304cb1637259d67
Esmeralda <esmeraldastarh@gmail.com>
2024-05-13 12:16:51 -0400

    Nueva version

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const menu__horizontal = document.querySelector('.menu__horizontal');
+const cierraMenu = document.querySelector('.cierraMenu');
+const abreMenu = document.querySelector('.abreMenu');
+
+
+abreMenu.addEventListener('click',show);
+cierraMenu.addEventListener('click',close);
+
+function show(){
+    menu__horizontal.style.display = 'flex';
+    menu__horizontal.style.left = '0';
+}
+function close(){
+    menu__horizontal.style.left = '120%';
+}
